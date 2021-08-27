9fdf67273df1c5b5abb3d7a494e823a158953456
Thiago Zilli <thiago.zilli@gmail.com>
2021-08-27 16:00:35 -0300

    Remove comments

--- main.go
+++ main.go
@@ -6,17 +6,6 @@ import (
 
 func main() {
 
-	// srv, err := server.NewServer(8181)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// srv.RegisterRoutes()
-	// srv.RegisterHTTP()
-
-	// if err := srv.Run(); err != nil {
-	// 	log.Fatal(err)
-	// }
-
 	cmd.Execute()
 
 }
